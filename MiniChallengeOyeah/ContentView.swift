f48a6414a94972bca0c0a8a65229ded7849cb714
Gregorius Yuristama <gregoriusyuristama@gmail.com>
2023-04-25 14:23:43 +0700

    Initial Commit

---
+++ MiniChallengeOyeah/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MiniChallengeOyeah
+//
+//  Created by Gregorius Yuristama Nugraha on 4/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniChallengeOyeah/MiniChallengeOyeahApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallengeOyeahApp.swift
+//  MiniChallengeOyeah
+//
+//  Created by Gregorius Yuristama Nugraha on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallengeOyeahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
