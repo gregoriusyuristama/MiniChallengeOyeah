1878b4b2c23067c3b37a08468210472cb197167a
syifa <syifanisha@gmail.com>
2023-05-02 10:44:23 +0700

    bikin file buat landing page

---
+++ Presenters/Views/LandingPage.swift
@@ -0,0 +1,20 @@
+//
+//  LandingPage.swift
+//  MiniChallengeOyeah
+//
+//  Created by Asyifa Tasya Fadilah on 02/05/23.
+//
+
+import SwiftUI
+
+struct LandingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandingPage_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingPage()
+    }
+}
