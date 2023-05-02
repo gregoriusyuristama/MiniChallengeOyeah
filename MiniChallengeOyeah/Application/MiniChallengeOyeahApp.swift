a4e2269771469843b7abe9900bed16b07238ac50
syifa <syifanisha@gmail.com>
2023-05-02 13:05:24 +0700

    tambahin landingpage dan categorytipspage, yang kurang di bagian text landing page (yang berubah tiap 3 detik)

--- MiniChallengeOyeah/Application/MiniChallengeOyeahApp.swift
+++ MiniChallengeOyeah/Application/MiniChallengeOyeahApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MiniChallengeOyeahApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LandingPage()
         }
     }
 }
--- Presenters/Views/LandingPage.swift
+++ Presenters/Views/LandingPage.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct LandingPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            GeometryReader { geo in
+                ZStack{
+                    Image("LandingPage")
+                        .resizable()
+                        .frame(width:geo.size.width, height:geo.size.height)
+                        .scaledToFit()
+                    
+                    VStack {
+                        Text("ahhaha")
+                            .frame(width: 120, height: 105)
+                            .offset(y: -230)
+                        
+                        HStack{
+                            NavigationLink(destination:CategoryTipsPage())
+                            {
+                                Circle()
+                                    .fill(.clear)
+                            }
+                            .frame(width: 120, height: 105)
+                            .offset(y: -200)
+                            .offset(x:-11)
+                            
+                            NavigationLink(destination:ContentView())
+                            {
+                                Circle()
+                                    .fill(.clear)
+                            }
+                            .frame(width: 120, height: 105)
+                            .offset(y: -200)
+                            .offset(x:-11)
+                        }
+                    }
+                }
+            }
+            .ignoresSafeArea()
+        }
     }
 }
 
