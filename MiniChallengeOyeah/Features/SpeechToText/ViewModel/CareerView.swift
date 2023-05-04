3b3fce3aebbbdc7f4dbb8d56886b230021db1c70
Brandon <itsbrandonlimm@gmail.com>
2023-05-04 12:25:03 +0700

    merapihkan MVVM

---
+++ MiniChallengeOyeah/Features/SpeechToText/ViewModel/CareerView.swift
@@ -0,0 +1,21 @@
+//
+//  CareerView.swift
+//  MiniChallengeOyeah
+//
+//  Created by Brandon Nicolas Marlim on 5/4/23.
+//
+
+import Foundation
+struct TipsCareer{
+    public static let tips: [String] = [
+        "It is a good idea to discuss your future and what you want to achieve in life with your parents because  they have likely encountered similar challenges that you're facing or the one that you are going to face",
+        
+        "When you are heading and working towards a personal goal, you can gain valuable insights and encouragement that can help you improve by asking for a feedback from your parents",
+        
+        "When you are doubting yourself or when you feel hesitate about the decision that you have made or the one that you are going to made, asking for advice and support from your parents can be an uplifting experience that can help you gain confidence",
+        
+        "Keep an open mind because you may find your parents have a different perspective.  Be patient, understanding, and respectful of their opinions and don't let it be the reason for you to stop communicating with them",
+        
+        "Mapping career plans for the next few years along with the best alternatives, such as companies that are targeted and targeted for promotion to increase the level of seriousness in career"].reversed()
+    private init(){}
+}
--- MiniChallengeOyeah/Features/Tips/Views/TipsFinancePage.swift
+++ MiniChallengeOyeah/Features/Tips/Views/TipsFinancePage.swift
@@ -8,16 +8,6 @@
 import SwiftUI
 
 struct TipsFinancePage: View {
-    private var tips: [String] = [
-        "Knowing the financial situation of the family in advance by taking an approach in the form of exchanging information so that the goals and effectiveness of a communication process are achieved",
-        
-        "Be open with parents about the financial problems they are experiencing and discuss to find the best solution to the problems they are facing",
-        
-        "Do some professional research and read basic references on personal financial planning and how to manage those finances",
-        
-        "Planning a retirement financial strategy by saving at the age of 20, managing budget circulation which is a priority in life, allocating assets, and managing risks based on needs",
-        
-        "Talk about how to arrange cash flow, ask them how to do a good calculating and comprehensive financial effectiveness ratios based on priority budgets"].reversed()
     
     var body: some View {
         GeometryReader{ geo in
@@ -38,7 +28,7 @@ struct TipsFinancePage: View {
                 .offset(x: 65)
 
                     
-                ForEach(tips, id:\.self) { info in
+                ForEach(TipsFinance.tips, id:\.self) { info in
                     CardFinanceView(info: info)
                         .padding(.bottom, 180)
 //                        .padding(.trailing, 27)
